482372545b52c40274a8611a588c742b0f3e8bfc
igormichalskii <imichalski@protonmail.com>
2026-06-01 16:29:53 +0200

    serviceworker fix

--- src/sw.js
+++ src/sw.js
@@ -2,9 +2,14 @@ import { precacheAndRoute } from 'workbox-precaching';
 
 precacheAndRoute(self.__WB_MANIFEST);
 
-self.addEventListener('push', function(event) {
+self.addEventListener('push', function (event) {
     const data = event.data.json()
-    self.registration.showNotification(data.title, {
-        body: data.body
-    })
+    let title = 'JARVIS'
+    let body = data
+    try {
+        const parsed = JSON.parse(data)
+        title = parsed.title
+        body = parsed.body
+    } catch { }
+    self.registration.showNotification(title, { body })
 })
